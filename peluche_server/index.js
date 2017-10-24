933346af6f1b084a5ffe917d05866a84e8861672
Alexis Metge <alexis.metge@etu.unice.fr>
2017-10-24 17:44:08 +0200

    Add simple POST /light on the server

--- peluche_server/index.js
+++ peluche_server/index.js
@@ -10,7 +10,7 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 server.post('/button', function (req, res, next) {
-
+    console.log("Button pressed");
     res.send(req.params);
     return next();
 });
@@ -19,3 +19,28 @@ server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
 
+var pi = {
+    addr: "localhost",
+    port: 7896
+};
+
+if (process.argv.length > 2)
+    pi.addr = process.argv[2];
+if (process.argv.length > 3)
+    pi.port = +process.argv[3];
+
+console.log("The ip is on " + pi.addr + ":" + pi.port);
+
+server.post('/light', (req, res, next) => {
+    fetch(pi.addr + ':' + pi.port + '/light', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => {
+        if (res.ok) {
+            console.log("Light success");
+        }
+    });
+    return next();
+});
