89c6f8940b11fc3a6ab1f1fce72715aaba273189
Unknown <fezai.ahmed@gmail.com>
2018-01-23 15:07:30 +0100

    modif

--- peluche_server/js/accelerometre.js
+++ peluche_server/js/accelerometre.js
@@ -1,15 +1,14 @@
 var data = "no data to display";
 
-function myTemperature() {
-    console.log('toto2');
+function myAccelerometre() {
     var http = new XMLHttpRequest();
-    var url = "http://192.168.1.158:7896/temperature";
+    var url = "http://192.168.1.158:7896/accelerometer";
 
-    http.open('GET', url, true);
+    http.open('GET', url, false);
     http.send(null);
 
     if (http.status === 200) {
-        console.log("La temperature s: %s", http.responseText);
+        console.log("La qualite dair est : %s", http.responseText);
         data = http.responseText;
     } else {
         console.log("Status de la réponse: %d (%s)", http.status, http.statusText);
--- peluche_server/js/temperature.js
+++ peluche_server/js/temperature.js
@@ -1,15 +1,14 @@
 var data = "no data to display";
 
-function myAccelerometre() {
-    console.log('toto');
+function myTemperature() {
     var http = new XMLHttpRequest();
-    var url = "http://192.168.1.158:7896/accelerometer";
+    var url = "http://192.168.1.158:7896/temperature";
 
-    http.open('GET', url, true);
+    http.open('GET', url, false);
     http.send(null);
 
     if (http.status === 200) {
-        console.log("La qualite dair est : %s", http.responseText);
+        console.log("La temperature s: %s", http.responseText);
         data = http.responseText;
     } else {
         console.log("Status de la réponse: %d (%s)", http.status, http.statusText);
